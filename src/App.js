1653b39ba0248ed910f5866335f49d0fbd063d40
Larissa <nuneslari21@gmail.com>
2021-10-25 12:23:43 -0300

    homem-feliz

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import img from './assets/homem-feliz-dinheiro.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={img} alt="homem-feliz" />
+        <p>
+         Homem feliz!!!
+      </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
